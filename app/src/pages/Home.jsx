3bf605fc925e2b91fe26b47d3dcbe37b0b4bff2a
damians1976 <damian_shuttle@hotmail.com>
2024-11-08 06:21:42 +0000

    app update

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,9 +1,62 @@
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import { FirestoreContext } from "../contexts/FirestoreContext"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+import {collection, getDocs} from "firebase/firestore"
 
-export function Home ( props ) {
-  const db = useContext (FirestoreContext)  
+export function Home(props) {
+  
+  const [bookdata,setBookData] = useState([])
+  let booksLoaded = false
+  
+  const db = useContext(FirestoreContext)
+
+  const getBooks = async () => {
+    const booksCollection = collection( db, "books") 
+    const result = await getDocs( booksCollection )
+    let booksArray = []
+    result.forEach( (doc) => {
+      let book = doc.data()
+      book.id = doc.id
+      booksArray.push( book )
+    } )
+    setBookData( booksArray )
+  }
+
+  useEffect( () => {
+    if( booksLoaded == false ){
+      getBooks()
+      booksLoaded = true
+    } 
+
+  }, [booksLoaded])
+
+  const Books = bookdata.map(( book ) => {
+    return{
+      <Col md={3}>
+        <Card>
+          <Card.Body>
+            <Card.Title>{book.title} </Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+    }
+  })
+
   return (
-      <h1>Home</h1>
-    )
+    <Container fluid>
+      <Row>
+        <Col>
+          <h1>Library System</h1>
+        </Col>
+      </Row>
+      <Row>
+      {/*books here*/}
+      { Books }
+      </Row>
+    </Container>
+  )
 }
