31eb2530dd8694838ede37e0fb89bb60f58e1824
damians1976 <damian_shuttle@hotmail.com>
2024-11-16 20:16:16 +0000

    app update

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
+export function Footer ( props ){
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <h5>Footer</h5>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
--- app/src/pages/BookDetail.jsx
+++ app/src/pages/BookDetail.jsx
@@ -2,16 +2,50 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { useParams } from 'react-router-dom'
+import { useState, useEffect, useContext } from 'react'
+import { FirestoreContext } from '../contexts/FirestoreContext'
+import { doc, getDoc } from 'firebase/firestore'
 
 export function BookDetail( props ) {
-    return(
+    const[ book, setBook ] = useState()
+    
+    const params = useParams()
+
+    const db = useContext( FirestoreContext )
+    const getBookDetail = async ( id ) => {
+        const ref = doc( db, "books", params.bookId)
+        const detail = await getDoc( ref )
+        let bookObject = detail.data()
+        bookObject.id = detail.id
+        setBook( bookObject )
+    }
+
+    useEffect( () => {
+        getBookDetail()
+    }, [ book ])
+
+    if ( book ) {
+        document.title = book.title
+    
+        return(
         <Container>
             <Row>
-                <Col>
-                    <h1>Book Detail</h1>
+                <Col md={12}>
+                    <h1>{ book.title }</h1>
+                </Col>
+                <Col md={6}>
+                    <img className="w-100" src={"book_covers" + book.cover} />
+                </Col>
+                <Col md={6}>
+                <p>{book.title} by { book.author }</p>
                 </Col>
             </Row>
 
         </Container>
+    )
+}
+else{
+    return( 
+        <p>...Loading</p>
     )
 }
